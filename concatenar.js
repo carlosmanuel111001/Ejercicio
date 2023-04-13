c60b759c1d6e80a15d4f8741eec6354af7b4a2f4
carlosmanuel111001 <carlos1110fuentes@gmail.com>
2023-04-12 18:32:22 -0600

    Tarea de javascript

---
+++ concatenar.js
@@ -0,0 +1,10 @@
+function concatenarCadenas(cadena1, cadena2) {
+  return cadena1 + cadena2;
+}
+
+// Ejemplo de uso
+var cadena1 = "Hola, ";
+var cadena2 = "mundo!";
+var resultado = concatenarCadenas(cadena1, cadena2);
+console.log(resultado); // Imprimirá "Hola, mundo!"
+
